818ad9532374bb9964797990cbd377401bac2db6
JavaDonas <gustavonazaro@uni9.edu.br>
2026-03-01 20:06:12 -0300

    Segunda questão resolvida

--- Exercício-02.py
+++ Exercício-02.py
@@ -1,6 +1,10 @@
 # Declaração de variáveis 
+salário: float = 0.0
 
 
 # Início 
+
+salário = float(input('Qual é o seu salário? '))
+print('Com o novo reajuste, seu salário atual é de ', salário * 1.15)
 
 # Fim
