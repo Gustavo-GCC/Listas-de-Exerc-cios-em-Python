1736b1b22532f450b2b711ac4b671bc1339d4167
Gustavo-GCC <gutoonxoffi22@gmail.com>
2026-03-02 12:48:07 -0300

    Questão 4 e 5 finalizadas com sucesso

--- Exercício-04.py
+++ Exercício-04.py
@@ -1,6 +1,11 @@
 # Declaração de variáveis 
-
+celsius: float = 0.0
+fahrenheit: float = 0.0
 
 # Início 
+
+celsius = float(input('Qual é a temperatura em graus celsius? '))
+fahrenheit = (9 * celsius + 160) / 5
+print('Então essa mesma temperatura em fahrenheit é de', fahrenheit, 'graus.')
 
 # Fim
--- Exercício-05.py
+++ Exercício-05.py
@@ -1,6 +1,30 @@
+# Importação de bibliotecas
+import math
+
 # Declaração de variáveis 
+a: float = 0.0
+b: float = 0.0
+c: float = 0.0
+x: float = 0.0
+DISCRIMINANTE: float = 0.0
 
 
 # Início 
+
+a = float(input('Qual é o valor do coeficiente quadrático? '))
+b = float(input('Qual é o valor do coeficiente linear? '))
+c = float(input('Qual é o valor do termo independente? '))
+DISCRIMINANTE = math.pow(b, 2) - 4*a*c
+
+if DISCRIMINANTE < 0:
+    print('As raízes da equação não pertencem ao conjunto dos números reais.')
+elif DISCRIMINANTE == 0:
+    x = (-b) / 2*a
+    print('A única raiz da equação é', x)
+else:
+    x = ((-b) - math.sqrt(DISCRIMINANTE)) / 2*a
+    print('A primeira raiz é', x)
+    x = ((-b) + math.sqrt(DISCRIMINANTE)) / 2*a
+    print('E a segunda raiz é', x)
 
 # Fim
