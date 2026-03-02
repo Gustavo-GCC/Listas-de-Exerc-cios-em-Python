71570d12a6f2755cb5a4d10da6800506c6439d9b
JavaDonas <gustavonazaro@uni9.edu.br>
2026-03-02 12:19:18 -0300

    Terceira questão feita

--- Exercício-03.py
+++ Exercício-03.py
@@ -1,6 +1,11 @@
 # Declaração de variáveis 
-
+altura: float = 0.0
+base: float = 0.0
 
 # Início 
+
+altura = float(input('Qual é a altura do triângulo? '))
+base = float(input('Qual é a base do triângulo? '))
+print('Assim sendo, sua área é de', altura * base / 2, 'unidades quadradas.')
 
 # Fim
